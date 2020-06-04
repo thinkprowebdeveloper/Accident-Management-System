b4b22b177efdba578e8db385ce5cbad32d3c27e5
thinkprowebdeveloper <okehiepaulchima@gmail.com>
2020-06-04 14:13:08 +0100

    My First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',() => {
+        // navv toggle
+        nav.classList.toggle('nav-active'); 
+
+        // animate links
+        navlinks.forEach((link, index) => {
+          if (link.style.animation) {
+           link.style.animation = '';
+           } else {
+               link.style.animation = 'navlinkfade 0.5s ease forwards ${index / 7 + 0.3}s';
+           }
+       });
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide(); 
